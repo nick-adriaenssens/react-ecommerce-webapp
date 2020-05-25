58cf610a20dfca4587aac342df44060cff7e0b18
Nick Adriaenssens <nick.adriaenssens@gmail.com>
2020-05-25 18:21:41 +0200

    Section 4: homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import HomePage from "./homepage/homepage.component";
 
 function App() {
   return (
     <div>
-      Hello World
+        <HomePage/>
     </div>
   );
 }
